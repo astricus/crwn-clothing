9e11b4d921f7ba9e33e6211a709da37874edb9de
astricus <astricus@hotmail.com>
2020-02-19 18:50:42 +0300

    adding server.js and stripe backend

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
-    const priceForStipe = price * 100;
+    const priceForStripe = price * 100;
     const publishableKey = 'pk_test_Sm4BFcoXrKV7BbB5h2WAiJx000l0C8PDWX';
 
     const onToken = token => {
-        console.log(token);
-        alert('Payment successful');
-    };
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     
     return (
         <StripeCheckout
@@ -18,7 +34,7 @@ const StripeCheckoutButton = ({ price }) => {
             shippingAddress
             image='https://svgshare.com/i/CUz.svg'
             description={`Your total is $${price}`}
-            amount={priceForStipe}
+            amount={priceForStripe}
             panelLabel='Pay Now'
             token={onToken}
             stripeKey={publishableKey}
